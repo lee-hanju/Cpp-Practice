e0174bef6dd2e963570959b70ec3175458d5ae9a
lee-hanju <codeup000611@gmail.com>
2020-01-28 22:38:27 +0900

    baekjoon_10817

---
+++ baekjoon_10817/baekjoon_10817.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a>=b)
+	{
+		if (b>=c)
+		{
+			cout << b;
+		}
+		else if (a >= c) 
+		{
+			cout << c;
+		}
+		else
+		{
+			cout << a;
+		}
+	}
+	else if (a>c)
+	{
+		cout << a;
+	}
+	else if(c>b)
+	{
+		cout << b;
+	}
+	else
+	{
+		cout << c;
+	}
+}
