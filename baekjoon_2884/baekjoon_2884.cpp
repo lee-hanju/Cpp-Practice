6090a7db93c4605ecf63ec6279d5143b9cc538f8
lee-hanju <codeup000611@gmail.com>
2020-02-06 21:16:50 +0900

    baekjoon_2884

---
+++ baekjoon_2884/baekjoon_2884.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int H, M;
+	cin >> H >> M;
+	if (H>=0 && H <= 23)
+	{
+		if (M>=0 && M<=59)
+		{
+			if (M-45 >= 0)
+			{
+				cout << H << " " << M - 45;
+			}
+			else
+			{
+				cout << H - 1 << " " << 60 + (M - 45);
+			}
+		}
+	}
+}
